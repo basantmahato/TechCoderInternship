fd79af80d989b2236f19114dc40cd01e1c82f21d
Basant Kumar Mahato <basantkumar72580@gmail.com>
2025-09-10 16:37:27 +0530

    Day 2

--- React/reactbits/src/main.jsx
+++ React/reactbits/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import SplitText from './SplitText.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SplitText text="Basant Mahato" />
   </StrictMode>,
 )
